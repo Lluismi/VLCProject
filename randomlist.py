b521bfecf9b6b45c7056efe98b7b036c405fa13e
Lluismi <lmorey@cifpfbmoll.eu>
2019-11-21 19:48:47 +0100

    upgrade random

--- randomlist.py
+++ randomlist.py
@@ -1,17 +1,8 @@
+import random
 import xml.etree.ElementTree as ET
-tree = ET.parse('prueba.xml')
+tree = ET.parse('Library.xml')
 root = tree.getroot()
 
-
 for child in root:
-    print(child.tag, child.attrib)
-
- # libreria = {"California_Uber_Alles.mp3":
-    #           {"track-number": 3, "artist": "Dead Kennedys", "album": "Dead Kennedys", "location": "./biblioteca/California_Uber_Alles.mp3"},
-    #        "Seattle_Party":
-    #            {"track-number": 1, "artist": "Chastity Belt", "album": "No regrets", "location": "./biblioteca/Seattle_Party.flac"},
-    #        "King_Kunta":
-    #            {"track-number": 3, "artist": "Kendrick Lamar", "album": "To Pimp A Butterfly", "location": "./biblioteca/King_Kunta.mp3"}
-    #        }
-    # random.choice(libreria.keys())
-    # return libreria
+    songs = child.tag('canciones') 
+    print (child.tag, child.attrib)
