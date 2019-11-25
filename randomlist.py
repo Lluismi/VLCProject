440eccb46bbbebe9eabf06a3415df0936cee5f44
Lluismi <56043249+Lluismi@users.noreply.github.com>
2019-11-25 20:13:43 +0100

    Add files via upload

--- randomlist.py
+++ randomlist.py
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
+import xml.etree.ElementTree as ET 
+
+tree = ET.parse('Library.xml')
+root = tree.getroot()
+
+def lista(root):
+    song = []
+    for track in root.iter('track'):
+        song.append(track.attrib)
+print(lista)
+=======
 import random
 import xml.etree.ElementTree as ET
 tree = ET.parse('Library.xml')
@@ -7,3 +19,4 @@ root = tree.getroot()
 for track in root.iter('track'):
     songs = []
     songs.append()
+>>>>>>> 58323d737e8c37545dfe3d2d2cad038716a6bc9a
