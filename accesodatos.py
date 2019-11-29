d3f5f25bd2466932f4e3be3bdf82db8852c3a1e1
Lluismi <lmorey@cifpfbmoll.eu>
2019-11-29 16:12:20 +0100

    upgrade

--- accesodatos.py
+++ accesodatos.py
@@ -1,23 +1,31 @@
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
 
 
 def listaidcanciones():
+    try:
         tree = ET.parse('Library.xml')
-        root = tree.getroot()
-        idsong = []
-        for track in root.find('cancions'):
-                for id in track.findall('id'):
-                        idsong.append(id.text)
-        return idsong
+    except ET.ParseError:
+        print("No ha encontrado el xml")
+    root = tree.getroot()
+    idsong = []
+    for track in root.find('cancions'):
+        for id in track.findall('id'):
+            idsong.append(id.text)
+    return idsong
+
 
 def pathcanciones():
+    try:
         tree = ET.parse('Library.xml')
-        root = tree.getroot()
-        pathsong = []
-        for track in root.find('cancions'):
-                for path in track.findall('path'):
-                        pathsong.append(path.text)
-        return pathsong
+    except ET.ParseError:
+        print("No ha encontrado el xml")
+    root = tree.getroot()
+    pathsong = []
+    for track in root.find('cancions'):
+        for path in track.findall('path'):
+            pathsong.append(path.text)
+    return pathsong
+
 
 rutacancionesid = {}
 
