1f7f4bee73571aa938878ea5b1817defa644c826
Lluismi <lmorey@cifpfbmoll.eu>
2019-11-29 16:16:46 +0100

    Upgrade archivos

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import subprocess
 from subprocess import Popen
 from logica import randomskere
 
+
 def listapath():
     path = randomskere()
     rutavlc = " ".join(path)
     vlc = "C:/Program Files/VideoLAN/VLC/vlc.exe"
     subprocess.Popen([vlc, [rutavlc]])
+
 
 listapath()
