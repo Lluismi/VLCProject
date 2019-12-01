a663fae3b304274192ec9501ec304c062ce551f1
lluismi <lmorey@cifpfbmoll.eu>
2019-12-01 10:13:47 +0100

    fix: Hemos añadido un try except para validar la ruta
    Try except añadido para validar la ruta del vlc

--- sistemas.py
+++ sistemas.py
@@ -5,7 +5,11 @@ from logica import randompath
 
 
 def main():
+    try:
+        "C:/Program Files/VideoLAN/VLC/vlc.exe"
+    except FileNotFoundError:
+        print("No se ha encontrado el vlc.exe en la ruta indicada")
+    vlc = "C:/Program Files/VideoLAN/VLC/vlc.exe"
     path = randompath()
     rutavlc = " ".join(path)
-    vlc = "C:/Program Files/VideoLAN/VLC/vlc.exe"
     subprocess.Popen([vlc, [rutavlc]])
