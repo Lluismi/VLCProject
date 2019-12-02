86671f51ba844c9d2877f9d82f399c5447d93b0b
lluismi <lmorey@cifpfbmoll.eu>
2019-12-02 11:12:34 +0100

    fix: añadido caso test

--- test_datos.py
+++ test_datos.py
@@ -1,10 +1,12 @@
 from accesodatos import listaidcanciones
 from accesodatos import pathcanciones
-
+from accesodatos import rutacancionesid
 
 def test_listaid():
     assert len(listaidcanciones()) == 50
 
-
 def test_listapath():
     assert len(pathcanciones()) == 50
+
+def test_diccionario():
+    assert len(rutacancionesid) == 50
